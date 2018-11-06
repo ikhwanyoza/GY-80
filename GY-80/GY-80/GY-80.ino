24ca4e3593e1265eabf7d71f9aaaa790c6432879
ikhwanyoza <ikhwanyoza@gmail.com>
2018-11-06 21:41:57 +0700

    removing some uneeded comment

--- GY-80/GY-80/GY-80.ino
+++ GY-80/GY-80/GY-80.ino
@@ -5,28 +5,8 @@
 /* Assign a unique ID to this sensor at the same time */
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
-/*
-void displaySensorDetails(void)
-{
-  sensor_t sensor;
-  accel.getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" m/s^2");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" m/s^2");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" m/s^2");  
-  Serial.println("------------------------------------");
-  Serial.println("");
-  delay(500);
-}
-*/
-
 void setup(void) {
-  Serial.begin(9600);
-//  Serial.println("GY-80 Sensor Test"); Serial.println("");
-  
+  Serial.begin(9600);  
   /* Initialise the sensor */
   if(!accel.begin())
   {
@@ -40,9 +20,6 @@ void setup(void) {
   // accel.setRange(ADXL345_RANGE_8_G);
   // accel.setRange(ADXL345_RANGE_4_G);
   // accel.setRange(ADXL345_RANGE_2_G);
-  
-  /* Display some basic information on this sensor */
-//  displaySensorDetails();
   Serial.println("");
 }
 
